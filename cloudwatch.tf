8c2389d3e9359367029b8dda50a24d3dc01f6826
Jonathon Brouse <jonathon.brouse@spartasystems.com>
2016-12-01 22:13:40 +0000

    Added comment to cloudwatch

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -3,6 +3,7 @@ resource "aws_cloudwatch_log_group" "cloudwatch_log_group" {
   name = "${var.aws_account}-cloudtrail"
 }
 
+// Create Clouwatch Log Stream
 resource "aws_cloudwatch_log_stream" "cloudwatch_log_stream" {
   name           = "${var.aws_account}-cloudtrail-cloudwatch-log-stream"
   log_group_name = "${aws_cloudwatch_log_group.cloudwatch_log_group.name}"
