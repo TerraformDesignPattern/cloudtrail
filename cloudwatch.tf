5c86c1bbe2a94d9a262127152b513c0a80b4f39c
Jonathon Brouse <jonathon.brouse@spartasystems.com>
2016-12-01 22:12:26 +0000

    Added cloudwatch log streamd

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -2,3 +2,8 @@
 resource "aws_cloudwatch_log_group" "cloudwatch_log_group" {
   name = "${var.aws_account}-cloudtrail"
 }
+
+resource "aws_cloudwatch_log_stream" "cloudwatch_log_stream" {
+  name           = "${var.aws_account}-cloudtrail-cloudwatch-log-stream"
+  log_group_name = "${aws_cloudwatch_log_group.cloudwatch_log_group.name}"
+}
